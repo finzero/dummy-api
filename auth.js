39b7b78788ddd586750930ab970619cbe25cb7f5
Fazrin <fazrin.mutaqin@wgs-id.com>
2022-09-05 12:03:56 +0700

    Initial Commit - Dummy API

---
+++ auth.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+
+function base_response(statusCode, message, body) {
+  return {
+    errorCode: statusCode,
+    errorMessage: message,
+    body: body,
+  };
+}
+
+app.post('/login', (req, res) => {
+  const { userId, password } = req.body;
+  if (userId === undefined || password === undefined) {
+    return res.status(400).json(base_response('01', 'Bad Request'));
+  }
+
+  if (userId !== 'admin' && password !== 'admin') {
+    return res
+      .status(400)
+      .json(base_response('01', 'userId or Password Invalid'));
+  }
+
+  setTimeout(() => {
+    return res.json(
+      base_response('00', 'success', {
+        userId: userId,
+        token: Buffer.from('thisIsEncodedTokenString').toString('base64'),
+        refreshToken: Buffer.from('thisIsEncodedRefreshTokenString').toString(
+          'base64'
+        ),
+        expires: 3600,
+      })
+    );
+  }, 2000);
+});
+
+app.post('/revoke', (req, res) => {
+  return res.json(base_response('00', 'Token has been revoked'));
+});
+
+module.exports = app;
---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const PORT = 8000;
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'api-doc')));
+
+const auth = require('./auth');
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.use('/api/auth', auth);
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
